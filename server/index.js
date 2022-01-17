194d0f96d645fbb98c81ab1e301cce4fd1db4db6
Tobiillz <ayoadeopeoluwa@yahoo.com>
2022-01-17 12:06:47 +0100

    added cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const mysql = require('mysql')
+const cors = require('cors')
+
+app.use(cors());
 
 const db = mysql.createConnection({
     user:'root',
